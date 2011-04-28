4516bd9aa8b34714ed6c3c3ea36167263b6b8a6e
Ryan Gochee <rgochee@gmail.com>
2011-04-28 08:47:08 -0700

    Updating database config with our config values

--- application/config/database.php
+++ application/config/database.php
@@ -42,9 +42,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['username'] = 'cs130@rgochee.com';
+$db['default']['password'] = 'Eggert130';
+$db['default']['database'] = 'rgochee_cs130';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
