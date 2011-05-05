a36b093e09c3b875830106234d3039bf590a6eae
Ryan Gochee <rgochee@gmail.com>
2011-05-05 00:46:27 -0700

    Adding support for all types of inputs in forms. Also making required fields stand out

--- application/views/fill_form.php
+++ application/views/fill_form.php
@@ -6,11 +6,27 @@
 <form method="post">
 <?php foreach ($form->fields as $field): ?>
 <div class="field">
-	<label for="fields<?php echo $field->id; ?>" class="field_label"><?php echo $field->name; ?></label>
-	<?php if ($field->type="text"): ?>
-		<input id="fields<?php echo $field->id; ?>" name="fields[<?php echo $field->id; ?>]" type="text" value="" size="50" />
-	<?php else: ?>
-		not a text field!
+	<label for="fields<?php echo $field->id; ?>" class="field_label<?php if ($field->required) { echo ' field_required'; } ?>"><?php echo $field->name; ?></label>
+	<?php if ($field->type=="checkbox"): ?>
+		<?php foreach ($field->options->getOptions() as $option): ?>
+			<div class="subfield">
+			<input id="fields<?php echo $field->id; ?>" name="fields[<?php echo $field->id; ?>]" type="checkbox" value="<?php echo $option; ?>" /> <?php echo $option; ?>
+			</div>
+		<?php endforeach ?>
+	<?php elseif ($field->type=="radio"): ?>
+		<?php foreach ($field->options->getOptions() as $option): ?>
+			<div class="subfield">
+			<input id="fields<?php echo $field->id; ?>" name="fields[<?php echo $field->id; ?>]" type="radio" value="<?php echo $option; ?>" /> <?php echo $option; ?>
+			</div>
+		<?php endforeach ?>
+	<?php elseif ($field->type=="dropdown"): ?>
+		<select id="fields<?php echo $field->id; ?>" name="fields[<?php echo $field->id; ?>]">
+		<?php foreach ($field->options->getOptions() as $option): ?>
+			<option value="<?php echo $option; ?>" /> <?php echo $option; ?></option>
+		<?php endforeach ?>
+		</select>
+	<?php else: // assume it's a text input ?>
+		<input id="fields<?php echo $field->id; ?>" name="fields[<?php echo $field->id; ?>]" type="text" size="50" />
 	<?php endif ?>
 </div>
 <?php endforeach ?>
