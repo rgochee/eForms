21c168313de7c792bebe8cde87b7c8fbadba538a
coolgangsters <cool.gangsters@gmail.com>
2011-05-04 23:38:01 -0700

    Edited application/views/fill_form.php via GitHub

--- application/views/fill_form.php
+++ application/views/fill_form.php
@@ -1,3 +1,19 @@
-<pre><?php print_r($form); ?></pre>
+<style type="text/css">
+th { background-color: #ccc; padding: 2px 5px; }
+td { padding: 2px 5px; }
+table, th, td { border: 1px solid #bbb; }
+#nodata { text-align: center; }
+</style>
 
-		<?php echo ('forms/fill/'.$form->name) ?>
+<h2><?php echo $form->name; ?></h2>
+
+<?php echo $form->description; ?></h2>
+
+		<?php foreach ($form->fields as $field): ?>
+			<label><?php echo $field->name; ?></label>
+<?php if ($field->type="text"): ?>
+<input class="field->label" name="fields[<?=$field->id?>]" type="text" value="" size="50" />
+<?php else: ?>
+	"not a text field!"
+<?php endif; ?>
+		<?php endforeach ?>
