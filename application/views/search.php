27d471e652b9803b080794c84a2e1305c3e4c807
coolgangsters <cool.gangsters@gmail.com>
2011-06-03 22:59:00 -0700

    Comments added

--- application/views/search.php
+++ application/views/search.php
@@ -1,4 +1,3 @@
-
 <style type="text/css">
 #forms_list { list-style: none; }
 .form_info, .last_mod { color: #555555; }
@@ -7,17 +6,22 @@
 .form_link:hover, .form_info a:hover { text-decoration: underline; } 
 </style>
 
+<?php /* Page Title */ ?>
 <h2>Search Results</h2>
 
+  <?php /* Retrieve query from search field in header */ ?>
   <?php $var = @$_GET['find']; ?>
 
+<?php /* Page numbers if 20+ forms */ ?>
 <?php echo $this->pagination->create_links(); ?>
 
 <ul id="forms_list">
 <?php echo form_error('find'); ?>
 <?php if (!empty($forms)): ?>
+<?php /* For each form, it tries to match the form name with the query, and if successful, it'll display the results accordingly */ ?>
 <?php foreach($forms as $form): ?>
-	<li class="form">
+	<li class="form"> 
+                <?php /* Lowercase both the form name and query */ ?>
 		<?php $urlName = '/' . str_replace(' ', '-', strtolower($form->name)); ?>
 		<?php $query = '/' . str_replace(' ', '-', strtolower($var)); ?>
 		<?php if(strlen(strstr($query,$urlName))>0): ?>  
@@ -25,6 +29,7 @@
 
 			<span class="last_mod">Last modified: <?php echo date('M d, Y', $form->time_created); ?></span>
 			<div class="form_info">
+                        <?php /* If this is an admin, it'll allow for editing and viewing data of filled out forms */ ?>
 			<?php if($this->session->userdata('admin')): ?>
 		<?php echo anchor(nice_form_uri('admin/edit/', $form->id, $form->name), 'edit', 'title="Edit the form"'); ?>
 		<?php echo anchor(nice_form_uri('admin/data/', $form->id, $form->name), 'data', 'title="View form responses"'); ?>
@@ -35,6 +40,7 @@
 	<?php endif ?>
 
 <?php endforeach ?>
+<?php /* In case no forms are found */ ?>
 <?php else: ?>
 	<li>No forms found!</li>
 <?php endif ?>
